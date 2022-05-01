2d63048904907ceafba54b0d4ac82ec1093c4649
ANOMALI <80420642+Ryomen-Sukuna@users.noreply.github.com>
2022-05-01 20:10:52 +0700

    small improvement

--- go.mod
+++ go.mod
@@ -5,19 +5,19 @@ go 1.18
 
 require (
 	github.com/PaulSonOfLars/gotgbot/v2 v2.0.0-rc.7
-	github.com/joho/godotenv v1.4.0
 	github.com/mmcdole/gofeed v1.1.3
-	github.com/roj1512/GitHubFeedBot v0.0.0-20211214125715-58df171af6bc
 	github.com/xeonx/timeago v1.0.0-rc4
 )
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.0 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/joho/godotenv v1.4.0
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mmcdole/goxpp v0.0.0-20200921145534-2f3784f67354 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/PaulSonOfLars/gotgbot/v2"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
 	"github.com/joho/godotenv"
 
 	"github.com/Ryomen-Sukuna/GoGitBot/workers"
@@ -27,17 +29,48 @@ func main() {
 		},
 	)
 	if err != nil {
-		log.Fatal(err)
+		panic("failed to create new bot: " + err.Error())
 	}
 
 	updater := ext.NewUpdater(nil)
+	dispatcher := updater.Dispatcher
+
+	dispatcher.AddHandler(handlers.NewCommand("start", start))
 
 	workers.StartWorkers(b)
 
 	err = updater.StartPolling(b, &ext.PollingOpts{DropPendingUpdates: true})
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to start polling: %v\n", err)
+	} else {
+		log.Println("Started Polling...!")
 	}
 
+	fmt.Printf("%s has been started...!\nMade by @Ryomen-Sukuna\n", b.User.Username)
 	updater.Idle()
+}
+
+func start(bot *gotgbot.Bot, ctx *ext.Context) error {
+	msg := ctx.EffectiveMessage
+	user_name := ctx.EffectiveUser.FirstName
+
+	if ctx.EffectiveChat.Type != "private" {
+		return ext.EndGroups
+	}
+
+	msg.Reply(
+		bot,
+		fmt.Sprintf(
+			"*Hi %v*,\n"+
+				"I am a simple bot that notifies you about GitHub activities on Telegram using [Go](https://go.dev)*",
+			user_name,
+		),
+		&gotgbot.SendMessageOpts{
+			ParseMode:                "Markdown",
+			ReplyToMessageId:         msg.MessageId,
+			AllowSendingWithoutReply: true,
+			DisableWebPagePreview:    true,
+		},
+	)
+	return ext.EndGroups
 }
